8f94a46da7f8b1d998c2870bcf5927b64269ca0f
VSulevskiy <sulevsky@gmail.com>
2015-05-06 10:19:06 +0300

    2

--- src/com/courses/io/Outputer.java
+++ src/com/courses/io/Outputer.java
@@ -9,22 +9,15 @@ import java.io.OutputStream;
  */
 public class Outputer {
 
-    // путь к файлу который будем читать и записывать
     private String filePath;
 
     public Outputer(String filePath) {
         this.filePath = filePath;
     }
 
-    // запись в файл используя OutputStream
     public void write(String st) throws IOException {
-        // инициализируем поток для вывода данных
-        // что позволит нам записать новые данные в файл
         OutputStream outputStream = new FileOutputStream(filePath);
-        // передаем полученную строку st и приводим её к byte массиву.
         outputStream.write(st.getBytes());
-        // закрываем поток вывода
-        // только после того как мы закроем поток данные попадут в файл.
         outputStream.close();
     }
 }
