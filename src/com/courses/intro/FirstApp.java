ee07c0ae5610ac0f7f3daa55989252b54eb159a3
VSulevskiy <sulevsky@gmail.com>
2015-05-06 10:19:42 +0300

    2

---
+++ src/com/courses/intro/FirstApp.java
@@ -0,0 +1,10 @@
+package com.courses.intro;
+
+/**
+ * Created by VSulevskiy on 05.05.2015.
+ */
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
