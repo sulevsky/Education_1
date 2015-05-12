bfcfb188a022482fff0bdcd564b6782a851422cc
VSulevskiy <sulevsky@gmail.com>
2015-05-12 10:20:08 +0300

    Concurrency tasks #2

--- src/com/courses/concurrency_100/threads_and_executors/scheduled_executors_7/ScheduledExecutorExample.java
+++ src/com/courses/concurrency_100/threads_and_executors/scheduled_executors_7/ScheduledExecutorExample.java
@@ -1,5 +1,7 @@
 package com.courses.concurrency_100.threads_and_executors.scheduled_executors_7;
 
+import java.sql.Time;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -13,15 +15,20 @@ public class ScheduledExecutorExample {
         Runnable task = new SimpleRunnable();
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         ScheduledFuture future = executor.schedule(task,3, TimeUnit.SECONDS);
-        TimeUnit.MILLISECONDS.sleep(1000);
+        TimeUnit.MILLISECONDS.sleep(1211);
         long remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
         System.out.printf("Remaining delay: %sms \n", remainingDelay);
+
+        Runnable periodicTask = new SimpleRunnable();
+        int initialDelay = 0;
+        int period = 1;
+        executor.scheduleAtFixedRate(periodicTask,initialDelay,period,TimeUnit.SECONDS);
     }
 
     public static class SimpleRunnable implements Runnable{
         @Override
         public void run() {
-            System.out.println("Scheduling: " + System.nanoTime());
+            System.out.println("Scheduling: " + new Date());
         }
     }
 
