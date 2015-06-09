07c1cff5cd8c978f0266155555468efe7f982891
VSulevskiy <sulevsky@gmail.com>
2015-06-09 19:41:24 +0300

    ArrayList

--- src/com/courses/exceptions_100/ListOfNumbers.java
+++ src/com/courses/exceptions_100/ListOfNumbers.java
@@ -3,6 +3,8 @@ package com.courses.exceptions_100;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -48,11 +50,13 @@ public class ListOfNumbers {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 //        ListOfNumbers listOfNumbers = new ListOfNumbers();
 //        listOfNumbers.writeList();
         Object[][] arr = new Object[][]{new String[2],{}};
         Arrays.stream(arr).forEach(System.out::println);
+        URL url = new URL("https://translate.google.com/?source=gtx_m#ru/en/%D0%BF%D0%BE%D0%B4%D1%82%D0%B2%D0%B5%D1%80%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5");
+        System.out.println(url);
 //        System.out.println(Arrays.toString(arr));
     }
 
