41cd05bdf08d37c1f89345df3aaf88c3cbce40d8
VSulevskiy <sulevsky@gmail.com>
2015-06-22 19:20:23 +0300

    Day 1 Simple calculator

--- src/com/courses/intro_1/SimpleCalculator.java
+++ src/com/courses/intro_1/SimpleCalculator.java
@@ -1,36 +1,40 @@
 package com.courses.intro_1;
 
-import java.util.Locale;
-import java.util.Scanner;
+import com.sun.deploy.util.ArrayUtil;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by VSulevskiy on 22.06.2015.
  */
 public class SimpleCalculator {
     public static void main(String[] args) {
-        double firstNumber;
-        double secondNumber;
-        System.out.println("Please input first number: ");
-        Scanner scanner = new Scanner(System.in);
-//        scanner.useLocale(Locale.ENGLISH);
-        firstNumber = scanner.nextDouble();
-        System.out.print("Your input ");
-        System.out.print(firstNumber);
-        System.out.println(" as a first number.");
+//        double firstNumber;
+//        double secondNumber;
+//        System.out.println("Please input first number: ");
+//        Scanner scanner = new Scanner(System.in);
+////        scanner.useLocale(Locale.ENGLISH);
+//        firstNumber = scanner.nextDouble();
+//        System.out.print("Your input ");
+//        System.out.print(firstNumber);
+//        System.out.println(" as a first number.");
+//
+//        System.out.println("Please input second number: ");
+//        secondNumber = scanner.nextDouble();
+//        System.out.print("Your input ");
+//        System.out.print(secondNumber);
+//        System.out.println(" as a second number.\n");
+//
+//        double sum = firstNumber + secondNumber;
+//        System.out.println("Sum is " + sum);
+//        double diff = firstNumber - secondNumber;
+//        System.out.println("Difference is " + diff);
+//        double mult = firstNumber * secondNumber;
+//        System.out.println("Multiplication is " + mult);
+//        double div = firstNumber / secondNumber;
+//        System.out.println("Division is " + div);
 
-        System.out.println("Please input second number: ");
-        secondNumber = scanner.nextDouble();
-        System.out.print("Your input ");
-        System.out.print(secondNumber);
-        System.out.println(" as a second number.\n");
 
-        double sum = firstNumber + secondNumber;
-        System.out.println("Sum is " + sum);
-        double diff = firstNumber - secondNumber;
-        System.out.println("Difference is " + diff);
-        double mult = firstNumber * secondNumber;
-        System.out.println("Multiplication is " + mult);
-        double div = firstNumber / secondNumber;
-        System.out.println("Division is " + div);
     }
 }
