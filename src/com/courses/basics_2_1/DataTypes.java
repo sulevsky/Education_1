8fadbd0533aa622a3cbd657e0e92d0b1e2093fe7
Vovka <sulevsky@gmail.com>
2015-06-23 21:45:08 +0300

    Simple commit

--- src/com/courses/basics_2_1/DataTypes.java
+++ src/com/courses/basics_2_1/DataTypes.java
@@ -1,8 +1,44 @@
 package com.courses.basics_2_1;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by Vladimir Sulevskiy on 23.06.2015.
  */
 public class DataTypes {
+
+    public static void main(String[] args) {
+        double dO = 0.8;
+        double dT = 0.7;
+        double dF = 0.1;
+        double dS = dT+dF;
+
+        System.out.println(Math.abs(dO - dS)<0.00000001);
+
+//        switch(str){
+//
+//            case 2.6:
+//                System.out.println("Monday");
+//                break;
+//            case 1:
+//                System.out.println("Tuesday");
+//                break;
+//            case 2:
+//                System.out.println("Wednesday");
+//                break;
+//            default:
+//                System.out.println("Other day");
+//        }
+//        System.out.println("End");
+
+
+
+
+
+
+
+    }
+
+
 
 }
