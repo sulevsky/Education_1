fea9dc212744be1a8d8632ca02d37f6ff58b3342
VSulevskiy <sulevsky@gmail.com>
2015-06-23 15:14:34 +0300

    Day 1 Simple calculator

--- src/com/courses/intro_1/Test.java
+++ src/com/courses/intro_1/Test.java
@@ -1,6 +1,7 @@
+package com.courses.intro_1;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -8,79 +9,56 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("mySimpleCalc: Welcome!");
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        BigDecimal num1 = new BigDecimal("0.00");
-        BigDecimal num2 = new BigDecimal("0.00");
-
-        System.out.println("Please, input the first number");
-
-        boolean resumeLoop = true;
+        BigDecimal num1 = null;
+        BigDecimal num2 = null;
 
         do {
-            try {
-
-                scanner.hasNextBigDecimal();
+            System.out.println("Please, input the first number");
+            if (scanner.hasNextBigDecimal()) {
                 num1 = scanner.nextBigDecimal();
-                resumeLoop = false;
                 System.out.println("Your input " + num1.toPlainString() + " as the first number");
-
-            }
-            catch (InputMismatchException e1) {
-                System.err.println("Incorrect entry, exception " + e1 + ". You must enter numbers. Try again.");
-                scanner.nextLine();
+            } else {
+                System.out.println("Incorrect entry, exception " + scanner.nextLine() + ". You must enter numbers. Try again.");
             }
         }
-        while (resumeLoop);
-
-
-
-
-        System.out.println("Please, input the second number.");
-
-        boolean resumeLoop2 = true;
+        while (num1!=null);
 
         do {
-            try {
-                if (scanner.hasNextBigDecimal()) {}
+            System.out.println("Please, input the second number.");
+            if (scanner.hasNextBigDecimal()) {
                 num2 = scanner.nextBigDecimal();
-
-                BigDecimal sum = (num1.add(num2));
-                sum = sum.setScale(8, RoundingMode.HALF_DOWN);
-
-                BigDecimal dif = (num1.subtract(num2));
-                dif = dif.setScale(8, RoundingMode.HALF_DOWN);
-
-                BigDecimal mul = (num1.multiply(num2));
-                mul = mul.setScale(8, RoundingMode.HALF_DOWN);
-
-                BigDecimal div = (num1.divide(num2, 8, RoundingMode.HALF_DOWN));
-
-                resumeLoop2 = false;
-
                 System.out.println("Your input " + num2.toPlainString() + " as the second number.");
-
-                System.out.println("Results:");
-                System.out.println("Sum is: " + sum.toPlainString());
-                System.out.println("Difference is: " + dif.toPlainString());
-                System.out.println("Multiplication is: " + mul.toPlainString());
-                System.out.println("Division is: " + div.toPlainString());
-
-                scanner.close();
-            }
-
-            catch (InputMismatchException e2) {
-                System.err.println("Incorrect entry, exception " + e2 + ". You must enter numbers. Try again.");
-                scanner.nextLine();
-            }
-
-            catch (ArithmeticException e3) {
-                System.err.println("You can't divide by zero, exception " + e3 + ". Input the second number again.");
-                scanner.nextLine();
+            } else {
+                System.out.println("Incorrect entry, exception " + scanner.nextLine() + ". You must enter numbers. Try again.");
             }
         }
+        while (num2!=null);
 
-        while (resumeLoop2);
+        BigDecimal sum = (num1.add(num2));
+        sum = sum.setScale(8, RoundingMode.HALF_DOWN);
+
+        BigDecimal dif = (num1.subtract(num2));
+        dif = dif.setScale(8, RoundingMode.HALF_DOWN);
+
+        BigDecimal mul = (num1.multiply(num2));
+        mul = mul.setScale(8, RoundingMode.HALF_DOWN);
+
+        String divOutput;
+        if(num2.intValue()!=0){
+            divOutput = (num1.divide(num2, 8, RoundingMode.HALF_DOWN)).toPlainString();
+        } else{
+            divOutput = "You can't divide by zero";
+        }
+
+        System.out.println("Results:");
+        System.out.println("Sum is: " + sum.toPlainString());
+        System.out.println("Difference is: " + dif.toPlainString());
+        System.out.println("Multiplication is: " + mul.toPlainString());
+        System.out.println("Division is: " + divOutput);
+
+        scanner.close();
         System.exit(0);
     }
 }
